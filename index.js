fd52d428edc69a63313477415b8b0036de008c60
Justin Peralta <justinperalta@Justins-MacBook-Air.local>
2022-05-28 22:23:00 -0500

    new file

---
+++ index.js
@@ -0,0 +1,108 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+
+inquirer
+  // list of questions to gather information for the readme
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe how to install project.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How this project should be used?'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license type.',
+      choices: ['MIT', 'APACHE_2.0', 'GPL_3.0',]
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Contributed to the project?'
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'testing was completed for this project?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Personal/proefessional email?'
+    }
+ 
+  ])
+  .then(res => {
+
+    fs.writeFile('sampleREADME.md', `# ${res.title}
+  ![badge](https://img.shields.io/badge/license-${res.license}-blue.svg)
+  
+  -------
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Description:
+  
+  ${res.description}
+  
+  ## Installation:
+  
+  ${res.installation}
+  
+  ## Usage:
+  
+  ${res.usage}
+  
+  ## Contributing:
+  
+  ${res.contributing}
+  
+  ## Testing:
+  
+  ${res.testing}
+  
+  ## License:
+  
+  ${res.license}
+  
+  ## Questions:
+  
+  Github Username: [${res.github}](https://github.com/${res.github})
+  
+  Contact me via email at: [${res.email}](mailto:${res.email})
+  `, (err) => {
+      if (err) { console.log(err) }
+      console.log('File Created!')
+    })
+
+
+  })
+  .catch(err => console.log(err))
+
---
+++ sampleREADME.md
@@ -0,0 +1,43 @@
+# Justin's ReadMe
+  ![badge](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+  -------
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Description:
+  
+  professional Dox
+  
+  ## Installation:
+  
+  follow this Video
+  
+  ## Usage:
+  
+  it should be used to make a readme
+  
+  ## Contributing:
+  
+  Justin
+  
+  ## Testing:
+  
+  yes
+  
+  ## License:
+  
+  MIT
+  
+  ## Questions:
+  
+  Github Username: [jstnprlta](https://github.com/jstnprlta)
+  
+  Contact me via email at: [jstnprlta90@gmail.com](mailto:jstnprlta90@gmail.com)
+  
